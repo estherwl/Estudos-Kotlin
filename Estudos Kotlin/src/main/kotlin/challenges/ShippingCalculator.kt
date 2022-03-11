e1c62a2ed75481c118aec6fe76fd89a97af3b02e
Esther Wyse de Lucena <64702147+estherwl@users.noreply.github.com>
2022-03-11 14:12:15 -0300

    exercicios sololearn

---
+++ Estudos Kotlin/src/main/kotlin/challenges/ShippingCalculator.kt
@@ -0,0 +1,32 @@
+package challenges
+
+/* You are working on a eCommerce website and need to make a shipping cost calculator based on the order amount.
+The store uses the following cost structure:
+For orders in the US:
+- $75+ orders have free shipping
+- orders less than $75 have a shipping fee of 10% of the total order amount.
+For international orders, there is a 15% shipping fee, with a maximum of $50. This means that the maximum shipping
+fee for an international order is $50.
+You need to complete the given shippingCost() function, which takes the order amount and a Boolean indicating whether
+the order is international or not, and returns the shipping cost for that order.
+The return amount should be a Double. */
+
+fun shippingCost(amount: Double, international: Boolean): Double {
+    var result = 0.0;
+    if(international){
+        result += amount * 0.15
+        if(result > 50) result = 50.0
+    } else {
+        if(amount >= 75) {
+            result = 0.0
+        } else {
+            result += amount * 0.10
+        }
+    }
+    return result
+}
+fun main(args: Array<String>) {
+    val total = readLine()!!.toDouble()
+    val international = readLine()!!.toBoolean()
+    println(shippingCost(total, international))
+}
