e19bce166ae33fec028a50d2c0ad8bb319ec93bf
estherwl <estherlucena@gmail.com>
2022-04-05 09:16:34 -0300

    sobreescreve toString

--- Estudos Kotlin/src/main/kotlin/estudos/alura/modelo/Endereco.kt
+++ Estudos Kotlin/src/main/kotlin/estudos/alura/modelo/Endereco.kt
@@ -4,4 +4,12 @@ class Endereco(
     var logradouro: String= "",
     var numero: Int = 0,
     var cidade: String = ""
-)
+){
+    override fun toString(): String {
+        return """Endereco(logradouro='$logradouro', 
+            numero=$numero, 
+            cidade='$cidade')""".trimIndent()
+    //""" mostra que é uma raw string, ele concatena o texto, mas cada linha
+    //nova é uma linha que ele realmente pulará ao mostrar o texto
+    }
+}
