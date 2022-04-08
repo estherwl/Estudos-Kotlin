2eec6e3ad689f3c8e2f84859c6d76793b6bb8d55
estherwl <estherlucena@gmail.com>
2022-04-08 14:23:31 -0300

    implementa autenticavel para cliente

--- Estudos Kotlin/src/main/kotlin/estudos/alura/modelo/Autenticavel.kt
+++ Estudos Kotlin/src/main/kotlin/estudos/alura/modelo/Autenticavel.kt
@@ -1,15 +1,18 @@
 package estudos.alura.modelo
 
+
+interface Autenticavel {
+
+    fun autentica(senha: Int): Boolean
+
+}
+
 //Não mantêm estados (valores)
 //Não são instanciadas (são implicitamente abstracts)
 //Podem ter properties, mas sempre abstracts. Possuem comportamento similar ao
 //de uma função: não armazenam estado, ao invés disso, devolvem valor
 
-interface Autenticavel {
-
-    //poderia ter implementação uma implementação geral para
-    //toda classe que implementasse a interface
-    //Porém, cuidar com informações sensiveis que poderiam existir nas properties,
-    //já que elas precisariam ser public
-    fun autentica(senha: Int): Boolean
-}
+//poderia ter implementação uma implementação geral para
+//toda classe que implementasse a interface
+//Porém, cuidar com informações sensiveis que poderiam existir nas properties,
+//já que elas precisariam ser public
--- Estudos Kotlin/src/main/kotlin/estudos/alura/modelo/Conta.kt
+++ Estudos Kotlin/src/main/kotlin/estudos/alura/modelo/Conta.kt
@@ -1,18 +1,12 @@
 package estudos.alura.modelo
 
-//construtor pode ter valores padrão, ex: class Conta(val titular: String="esther", val numero: Int)
-//ou seja, ao criar o objeto se não declarar outro valor, terá o valor padrão
 abstract class Conta(val titular: Cliente,
                      val numero: Int,
-){
+): Autenticavel by titular
+{
     var saldo = 0.0
         protected set
-    //saldo só pode ser modificado através da classe, já que set é protected
 
-    //object declaration(declara singletons) -> para usar dentro da classe usa keyword companion
-    // faz com que mantenhamos um valor que seja considerado como estático, porque ele não
-    // muda o valor desde o começo do programa até o fim, sendo membro da
-    // classe e apenas ela consegue fazer a sua manipulação
     companion object Contador{
         var total = 0
             private set
@@ -22,10 +16,22 @@ abstract class Conta(val titular: Cliente,
         total++
     }
 
-    fun deposita(conta: estudos.alura.modelo.Conta, valor: Double){
+    fun deposita(conta: Conta, valor: Double){
         if(valor > 0) conta.saldo += valor
     }
 
     @Throws(Exception::class)
     abstract fun saca(valor: Double)
 }
+//ao implementar Autenticavel by titular, é usado o design pattern delegation, sendo desnecessário
+//a sobreescrita do autentica, pois já entende que: return titular.autentica(senha)
+
+//construtor pode ter valores padrão, ex: class Conta(val titular: String="esther", val numero: Int)
+//ou seja, ao criar o objeto se não declarar outro valor, terá o valor padrão
+
+//saldo só pode ser modificado através da classe, já que set é protected
+
+//object declaration(declara singletons) -> para usar dentro da classe usa keyword companion
+// faz com que mantenhamos um valor que seja considerado como estático, porque ele não
+// muda o valor desde o começo do programa até o fim, sendo membro da
+// classe e apenas ela consegue fazer a sua manipulação
